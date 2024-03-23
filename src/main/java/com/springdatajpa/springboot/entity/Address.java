0944bc4f93da8a584a3a76ea914aea4ddb6b04fd
pjkumar6219 <pjkumar6219@gmail.com>
2024-03-24 01:06:02 +0530

    new fields added in Address

--- src/main/java/com/springdatajpa/springboot/entity/Address.java
+++ src/main/java/com/springdatajpa/springboot/entity/Address.java
@@ -17,6 +17,11 @@ public class Address {
     private String state;
     private String country;
     private String zipCode;
+    //    Added
+    private String state1;
+    private String country1;
+    private String zipCode1;
+
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "order_id", referencedColumnName = "id")
