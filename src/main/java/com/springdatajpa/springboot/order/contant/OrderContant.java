4fdfbdde2aa953b826c776b8f2ab27f00f13942d
pjkumar6219 <pjkumar6219@gmail.com>
2024-03-24 02:53:05 +0530

    Constant

--- src/main/java/com/springdatajpa/springboot/order/contant/OrderContant.java
+++ src/main/java/com/springdatajpa/springboot/order/contant/OrderContant.java
@@ -4,6 +4,7 @@ public class OrderContant {
     //2: extended assortment 3: handmade
     public final static Integer HANDMADE_THREE = 3;
 
+    public final static Integer HANDMADE_FOUR = 4;
     public static final String APPLICATION_VND_MS_EXCEL = "application/vnd.ms-excel";
 
     public static final String UTF_8 = "utf-8";
